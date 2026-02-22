fe3b9268298113116c6edab1bb4bbc2d95585dd8
Oshadha Kalhara <oshadhawedaguruge@gmail.com>
2026-02-22 12:44:32 +0530

    add role based access and validation

--- Backend/middleware/mockUser.js
+++ Backend/middleware/mockUser.js
@@ -1,10 +1,13 @@
 // backend/middlewares/mockUser.js
 export const mockUser = (req, res, next) => {
+  const role = req.headers["x-role"] || "user"; // default user
+
   req.user = {
-    _id: "64123456789abcdef1234567", // some dummy ObjectId
+    _id: "64123456789abcdef1234567",
     name: "Test User",
     email: "test@example.com",
-    role: "user"
+    role: role,
   };
+
   next();
 };
